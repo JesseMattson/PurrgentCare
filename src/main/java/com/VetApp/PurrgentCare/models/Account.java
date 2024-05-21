bbd986acf9f61930645aee4c3dfc772b62d35ddb
norwoje <jeffreysnorwood@gmail.com>
2024-05-21 10:25:23 -0400

    Created Account controller, service interface and implementation, repository. Still need to create tests and postman for accounts.

--- src/main/java/com/VetApp/PurrgentCare/models/Account.java
+++ src/main/java/com/VetApp/PurrgentCare/models/Account.java
@@ -2,10 +2,16 @@ package com.VetApp.PurrgentCare.models;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
 import java.util.List;
-
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 @Entity(name = "Account") // Specified that this is a DATABASE Entity
 public class Account {
 
@@ -13,8 +19,9 @@ public class Account {
     @Id // Primary Key
     @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto Generate Values
     private Integer id;
+    @Setter
     private Boolean active;
-
+    @Setter
     private Date dateCreated;
 
     @OneToMany(mappedBy = "account")
@@ -25,5 +32,11 @@ public class Account {
     private List<Pet> pets;
 
 
-
+    @Override
+    public String toString() {
+        return "Account [id=" + id + ", active=" + active + " , dateCreated=" + dateCreated + "]";
+    }
 }
+
+
+
