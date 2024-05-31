22759429b0312d42f769d0c14ea926aeab289145
norwoje <jeffreysnorwood@gmail.com>
2024-05-31 10:08:07 -0400

    Added parameterization to person controller URL

--- src/main/java/com/VetApp/PurrgentCare/controllers/PersonController.java
+++ src/main/java/com/VetApp/PurrgentCare/controllers/PersonController.java
@@ -13,33 +13,35 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000") // This enables CORS for this controller
 public class PersonController {
 
+    private static final String BASE_URL = "/persons";
+
     @Autowired // You don't need to initialize the object.
     PersonServiceInterface personService;
 
-    @GetMapping("/persons/{id}") // PathVariable is used to get ID from the URL
+    @GetMapping(BASE_URL + "/{id}") // PathVariable is used to get ID from the URL
     private Person getPerson(@PathVariable("id") Integer id) {
 
         return personService.getPerson(id);
     }
 
-    @GetMapping("/persons/all")
+    @GetMapping(BASE_URL )
     private List<Person> getAllPersons() {
 
         return personService.getAllPersons();
     }
 
-    @PostMapping("/persons/AddPerson")
+    @PostMapping(BASE_URL)
     private void addPerson(@RequestBody Person person) {
        personService.addPerson(person);
     }
 
-    @DeleteMapping("/persons/DeletePerson/{id}")
+    @DeleteMapping(BASE_URL + "/{id}")
     private void deletePerson(@PathVariable("id") Integer personId) {
         personService.deletePerson(personId);
 
     }
 
-    @PutMapping("/persons/UpdatePerson/{id}")
+    @PutMapping(BASE_URL + "/{id}")
     private Person updatePerson(@PathVariable("id")  Integer personId, @RequestBody Person person) {
         return personService.updatePerson(person, personId);
 
