60baa354b2131f2b5b2edc65ee841e7a2ad26a02
RandallsBagels <59258405+RandallKeur@users.noreply.github.com>
2024-05-31 09:07:57 -0400

    Cross Origins and changes to enable front-end functionality for pet and person features (#34)
    * addding some shit
    
    * add cors for pets as well
    
    ---------
    
    Co-authored-by: RandallKeur <108151926+RandallKeur7@users.noreply.github.com>

--- src/main/java/com/VetApp/PurrgentCare/controllers/PetController.java
+++ src/main/java/com/VetApp/PurrgentCare/controllers/PetController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000") // This enables CORS for this controller
 public class PetController {
 
     @Autowired
