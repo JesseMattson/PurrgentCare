afa741ef191cf28274bb5efad5b18b6235ee71d3
norwoje <jeffreysnorwood@gmail.com>
2024-05-31 10:17:05 -0400

    Updated URL for pets; updated postman collection to account for URL changes for pets and persons.

--- src/main/java/com/VetApp/PurrgentCare/controllers/PetController.java
+++ src/main/java/com/VetApp/PurrgentCare/controllers/PetController.java
@@ -14,31 +14,33 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000") // This enables CORS for this controller
 public class PetController {
 
+    private static final String BASE_URL = "/pets";
+
     @Autowired
     PetServiceInterface petService;
 
-    @GetMapping("/pet/{id}") // PathVariable is used to get ID from the URL
+    @GetMapping(BASE_URL + "/{id}") // PathVariable is used to get ID from the URL
     private Pet getPet(@PathVariable("id") Integer id) {
 
         return petService.getPet(id);
     }
-    @PostMapping("/pet/AddPet")
+    @PostMapping(BASE_URL )
     private void addPet(@RequestBody Pet pet) {
         petService.addPet(pet);
     }
 
-    @DeleteMapping("/pet/DeletePet/{id}")
+    @DeleteMapping(BASE_URL + "/{id}")
     private void deletePet(@PathVariable("id") Integer petId) {
         petService.deletePet(petId);
     }
 
 
-    @PutMapping("/pet/UpdatePet/{id}")
+    @PutMapping(BASE_URL + "/{id}")
     private Pet updatePet(@PathVariable("id")  Integer petId, @RequestBody Pet pet) {
         return petService.updatePet(pet, petId);
     }
 
-    @GetMapping("/pet/all")
+    @GetMapping(BASE_URL)
     private List<Pet> getAllPets() {
 
         return petService.getAllPets();
