12dc73522cc5f08ecbf9f0591286a87d90a64b5c
mattsje <jrm5534@gmail.com>
2024-08-21 08:04:26 -0400

    fix account interface id naming

--- src/main/java/com/VetApp/PurrgentCare/services/AccountServiceInterface.java
+++ src/main/java/com/VetApp/PurrgentCare/services/AccountServiceInterface.java
@@ -9,15 +9,15 @@ import java.util.List;
 public interface AccountServiceInterface {
 
 
-    AccountResponse getAccount(Integer personId);
+    AccountResponse getAccount(Integer accountId);
 
     List<AccountResponse> getAllAccounts();
 
     AccountResponse addAccount(AccountRequest accountRequest);
 
-    void deleteAccount(Integer personId);
+    void deleteAccount(Integer accountId);
 
-    AccountResponse updateAccount(AccountRequest accountRequest, Integer personId);
+    AccountResponse updateAccount(AccountRequest accountRequest, Integer accountId);
 
     AccountResponse accountToggle(Integer accountId);
 
