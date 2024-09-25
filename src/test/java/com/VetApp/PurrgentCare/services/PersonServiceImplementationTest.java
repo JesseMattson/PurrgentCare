467801bd5051532e653c8456f5f9808e37dd9174
norwoje <jeffreysnorwood@gmail.com>
2024-09-25 10:33:33 -0400

    getPerson_whenExist_returnOnePerson refactored to get id from fake person.Build success

--- src/test/java/com/VetApp/PurrgentCare/services/PersonServiceImplementationTest.java
+++ src/test/java/com/VetApp/PurrgentCare/services/PersonServiceImplementationTest.java
@@ -48,9 +48,8 @@ public class PersonServiceImplementationTest {
     @Test
     public void getPerson_whenExist_returnOnePerson() {
         // given
-
-        final var fakePersonId = fakeDataGenerator.generateRandomInteger();
         final var fakePerson = fakeDataGenerator.generateFakePerson();
+        final var fakePersonId = fakePerson.getId();
         final var fakePersonResponse = fakeDataGenerator.generateFakePersonResponse();
         given(mockPersonRepository.findById(fakePersonId)).willReturn(Optional.of(fakePerson));
         given(mockMapper.map(Optional.of(fakePerson), PersonResponse.class)).willReturn(fakePersonResponse);
