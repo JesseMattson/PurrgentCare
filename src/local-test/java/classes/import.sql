40cc01b20877198bd172d249c5ec7b9edfc06273
Randall Keur <rkeur7@gmail.com>
2024-11-05 08:55:40 -0500

    attempt again

---
+++ src/local-test/java/classes/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO Account(active,date_Created) VALUES(true, '2024-05-17');
+INSERT INTO Account(active,date_Created) VALUES(true, '2024-05-17');
+insert into Account(active,date_Created) VALUES(true, '2024-05-17');
+insert into Person(NAME, Account_ID) VALUES('Jesse',1);
+insert into Person(NAME, Account_ID) VALUES('Morgan',1);
+insert into Person(NAME, Account_ID) VALUES('Jeffrey',2);
+insert into Person(NAME, Account_ID) VALUES('Randall',3);
+INSERT INTO PET(AGE,GENDER,NAME,TYPE, Account_ID) VALUES(1,'Male','Pharaoh','Dog',1);
+INSERT INTO PET(AGE,GENDER,NAME,TYPE, Account_ID) VALUES(1,'Male','Sully','Cat', 2);
+
